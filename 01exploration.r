16f58f9389a0b2b877d452104e1c2308ae03d334
missdefying <toniathompson@gmail.com>
2016-02-02 15:49:58 -0600

    Update 01exploration.r

--- 01exploration.r
+++ 01exploration.r
@@ -1,27 +1,23 @@
-#install R postgreSQL package for db connectivity/querying
+## install R postgreSQL package for db connectivity/querying
 install.packages("RPostgreSQL")
 
-#load the package you just installed
+## load the package you just installed
 library(RPostgreSQL)
 
-#load the drivers and create the connection string
 drv <- dbDriver("PostgreSQL")
-dbname <- "kaggle"; dbuser <- "admin"; dbpass <- "<obscured>";
-dbhost <- "obscuredhost.com"; dbport <- 11111;
+dbname <- "database-name"; dbuser <- "database-user"; dbpass <- "password";
+dbhost <- "your-db-host.com"; dbport <- 11111;
 con <- dbConnect(drv, host=dbhost, port=dbport, dbname=dbname, user=dbuser, password=dbpass)
 
-#Set working directory
+## Set working directory
 setwd("~/Dropbox/Kaggle/titanic")
 
-#Import files & set file vars
+## Import files & set file vars
 train <- read.csv("train.csv")
 test <- read.csv("test.csv")
 
-## Add survived column to test set with value of 0--in order to combine test & train data must match
-test$Survived <- rep(0, 418)
-combi <- rbind(train, test)
 
-## insert training data into a table
+## insert your data into a table
 dbWriteTable(con, "titanic", train, row.names=FALSE)
 
 ###############################
@@ -31,16 +27,13 @@ dbWriteTable(con, "titanic", train, row.names=FALSE)
 ##############################
 
 
-#query data from database--one variable @ a time
-#fetch = put results in data frame; -1=fetch all results
-#make sure you use double quotes since the table will be created with double quotes for col names
+## query data from database--one variable @ a time
+## fetch = put results in data frame; -1=fetch all results
+## make sure you use double quotes since the table will be created with double quotes for col names
 train <- fetch(dbSendQuery(con, 'SELECT * from titanic'), n=-1)
-
-#make R treat age as numeric and survived as a factor (it can't be a numeric range)
 train$Age <- as.numeric(train$Age)
 train$Survived <- as.factor(train$Survived)
 
-#start creating barplots to see where there might be indicators
 age <- table(train$Survived, train$Age)
 barplot(age, xlab="Passenger Age", col=c("red","green"), legend = rownames(age))
 
